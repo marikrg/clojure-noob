33d9101bea302bfcf2870bdd9623c40f3e47e448
Mariana Kreisig <marianakreisig@gmail.com>
2018-06-27 18:12:59 -0300

    Add inc-maker and dec-maker functions

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -2,6 +2,29 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Returns \"I'm a little teapot!\"."
   [& args]
-  (println "I'm a little teapot!"))
+  "I'm a little teapot!")
+
+(defn inc-maker
+  "Create a custom subtractor"
+  [inc-by]
+  #(+ % inc-by))
+
+(defn dec-maker
+  "Create a custom decrementor"
+  [dec-by]
+  #(- % dec-by))
+
+(def inc100 (inc-maker 100))
+(def dec100 (dec-maker 100))
+
+(defn mapset
+  "Returns a set of a given map"
+  [&map]
+  (set map))
+
+(defn mapset
+  "Returns a set of a given map"
+  [f vect]
+  (set (map f vect)))
--- test/clojure_noob/core_test.clj
+++ test/clojure_noob/core_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [clojure-noob.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-main
+  (testing "-main should return I'm a little teapot"
+    (is (= (-main) "I'm a little teapot!"))))
+
+(deftest test-add-100-with-positive-arg
+  (testing "adds 100 to 3 should return 103."
+    (is (= (inc100 3) 103))))
+
+(deftest test-add-100-with-negative-arg
+  (testing "adds 100 to -3 should return 97."
+    (is (= (inc100 -3) 97))))
+
+(deftest test-dec-100-with-positive-arg
+  (testing "substracts 100 from 3 should return -97."
+    (is (= (dec100 3) -97))))
+
+(deftest test-dec-100-with-negative-arg
+  (testing "substracts 100 from -3 should return -103."
+    (is (= (dec100 -3) -103))))
+
+(deftest test-mapset
+  (testing "should return a set."
+    (is (= (mapset inc [1 1 2 2]) #{2, 3}))))
